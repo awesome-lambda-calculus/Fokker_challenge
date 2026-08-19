ac825d76f0ef83563fed5b320d727297677e2ab5
lengyijun <noreply@github.com>
2026-08-19 19:14:19 +0800

    M.only_one_var_used is redundant

--- FokkerChallenge/FokkerUndecided.lean
+++ FokkerChallenge/FokkerUndecided.lean
@@ -88,17 +88,16 @@ theorem fokkerUndecided_not_onePointBasis
 
 theorem mem_terms_fokker_lt_7_iff (M : Term String)
      (hm : M ∈ terms_fokker_lt_7) :
-     (M.every_bvar_used || M.no_duplicate || M.only_one_var_used || M.isNamedOfXY || M ∈ fokkerUndecidedTerms) = true := by
+     (M.every_bvar_used || M.no_duplicate || M.isNamedOfXY || M ∈ fokkerUndecidedTerms) = true := by
      native_decide +revert
 
 theorem not_basis_of_closed_lc_small_fokker_size (M : Term String)
     (hm : M.LC ∧ M.fv = ∅ ∧ M.fokker_size < 7) : not_basis M := by
     rw [closed_lc_iff_mem_gen_terms] at hm
     apply mem_terms_fokker_lt_7_iff at hm
-    have h : M.every_bvar_used \/ M.no_duplicate \/ M.only_one_var_used \/ M.isNamedOfXY \/ M ∈ fokkerUndecidedTerms := by grind
-    rcases h with h|h|h|h|h
+    have h : M.every_bvar_used \/ M.no_duplicate \/ M.isNamedOfXY \/ M ∈ fokkerUndecidedTerms := by grind
+    rcases h with h|h|h|h
     . exact not_reaches_K h
     . exact not_reaches_omega h
-    . exact only_one_var_used_not_reaches_S h
     . exact isNamedOfXY_not_basis h
     . exact BetaReductOfNamable_not_basis (fokkerUndecided_betaReductOfNamable  _ h)
